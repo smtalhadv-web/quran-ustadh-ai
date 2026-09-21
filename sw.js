a3f7f0fa66feda87098522406ec8a97c24fa64b9
smtalhadv-web <smtalhadv@gmail.com>
2026-09-21 12:27:43 +0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='quran-ustadh-v1';
+const ASSETS=['./','./index.html','./styles.css','./src/app.js','./src/data/quran.js','./src/services/quran-service.js','./src/services/storage-service.js','./src/services/learning-engine.js','./src/services/mock-recitation-service.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
